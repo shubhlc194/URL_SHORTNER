f32ae94545ee00b7fd473dbce6d99ceb366e0c44
shubhlc194 <shubhamkc194@gmail.com>
2025-09-10 17:18:14 +0530

    url-shortner

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.get('/api/create', (req, res)=>{
+    
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on https://localhost:3000");
+});
+
+
+
+// get-redirection
+//post- create-short-url
